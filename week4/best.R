55226d26c6c550fe9ab37be848d6931a853913f4
Sean Clarke <sean@nay-clarke.com>
2015-02-27 22:36:52 +0000

    best.R is a WIP - next step is to move to using indexes instead oc Column names

--- week4/best.R
+++ week4/best.R
@@ -5,11 +5,24 @@ best <- function(state, outcome) {
     
     ## Check that state and outcome are valid
     
-    validoutcomes <- c("heart attack", "heart failure", "pneumonia")
+    validoutcomes <- c("Heart Attack", "Heart Failure", "Pneumonia")
+    indexes <- c(15, 21, 27 )
     
     if (sum(state.abb == state, na.rm=TRUE) == 0) stop("invalid state")
     if (sum(validoutcomes == outcome, na.rm=TRUE) == 0) stop("invalid outcome")
     
+    
+    
+    outhead <- paste("Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.", gsub(" ", ".",outcome), sep="")
+    #outhead <- ""Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia""
     ## Return hospital name in that state with lowest 30-day death
     ## rate
+    
+    #result <- subset(oocm, State == state)["Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"]
+    #result <- subset(oocm, State == state)[outhead]
+    result <- subset(oocm, State == state, select = c("Hospital.Name", outhead))
+    result
 }
+
+
+###### Next thing to do is to use column index instead of Name
