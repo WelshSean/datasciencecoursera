87c895ff38dbb6cadfdca64a766c409158cd9066
Sean Clarke <sean@nay-clarke.com>
2015-02-28 21:40:50 +0000

    intermediate version that only works for Pneumonia

--- week4/best.R
+++ week4/best.R
@@ -1,7 +1,7 @@
 best <- function(state, outcome) {
     ## Read outcome data
     
-    oocm <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    oocm <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available")
     
     ## Check that state and outcome are valid
     
@@ -11,18 +11,19 @@ best <- function(state, outcome) {
     if (sum(state.abb == state, na.rm=TRUE) == 0) stop("invalid state")
     if (sum(validoutcomes == outcome, na.rm=TRUE) == 0) stop("invalid outcome")
     
-    
-    
+    names(indexes) <- validoutcomes
+    outcomeindex <- as.integer(indexes[outcome])
     outhead <- paste("Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.", gsub(" ", ".",outcome), sep="")
-    #outhead <- ""Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia""
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate
     
-    #result <- subset(oocm, State == state)["Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"]
-    #result <- subset(oocm, State == state)[outhead]
-    result <- subset(oocm, State == state, select = c("Hospital.Name", outhead))
-    result
+
+    result <- subset(oocm, State == state)[,c(2,outcomeindex)]
+    result <- result[complete.cases(result),]   # Remove incomplete rows
+    result[,2] <- as.numeric(result[,2])
+    best <- subset(result, get(outhead) == min(result[,2]))
+    best
+    sort(best)
+
 }
 
 
-###### Next thing to do is to use column index instead of Name
+ 
