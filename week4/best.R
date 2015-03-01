21616348a970a008f7a7b3218c4ec7a1a3020f47
Sean Clarke <sean@nay-clarke.com>
2015-03-01 10:37:42 +0000

    best.R now refactored totally to make less generic but simpler to understand. Also I was using the wrong columnsgit add best.R git add best.R ! It now works and passes the Coursera course unit tests :-)

--- week4/best.R
+++ week4/best.R
@@ -1,29 +1,46 @@
 best <- function(state, outcome) {
-    ## Read outcome data
-    
+    setwd("/home/sean/DataScience/datasciencecoursera/week4")
     oocm <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available")
     
     ## Check that state and outcome are valid
     
-    validoutcomes <- c("Heart Attack", "Heart Failure", "Pneumonia")
+    validoutcomes <- c("heart attack", "heart failure", "pneumonia")
     indexes <- c(15, 21, 27 )
     
     if (sum(state.abb == state, na.rm=TRUE) == 0) stop("invalid state")
     if (sum(validoutcomes == outcome, na.rm=TRUE) == 0) stop("invalid outcome")
     
-    names(indexes) <- validoutcomes
-    outcomeindex <- as.integer(indexes[outcome])
-    outhead <- paste("Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.", gsub(" ", ".",outcome), sep="")
+    ## Summarise Data - this could be done in a way that is more general, but deadlines got me :-)
     
-
-    result <- subset(oocm, State == state)[,c(2,outcomeindex)]
-    result <- result[complete.cases(result),]   # Remove incomplete rows
-    result[,2] <- as.numeric(result[,2])
-    best <- subset(result, get(outhead) == min(result[,2]))
+    
+    if (outcome == "heart attack"){
+        data <-subset(oocm, State == state)[,c(2,11)]
+    } else if (outcome == "heart failure"){
+        data <-subset(oocm, State == state)[,c(2,17)]
+    } else if (outcome == "pneumonia") {
+        data <-subset(oocm, State == state)[,c(2,23)]
+    }
+    
+    ### Rename columns
+    
+    names(data) <- c("Hospital", "Freq")
+    
+    ### Make column 2 numeric
+    
+    data[,2] <- as.numeric(data[,2])
+    
+    ### Remove rows where Freq is NA
+    
+    data <- data[complete.cases(data),]   # Remove incomplete rows
+    
+    #### Now pick out rows where Freq == min(freq)
+    
+    best <- subset(data, Freq == min(data["Freq"]))
+    
+    ### Handle cases where there are more than 1 occurrences
+    
+    best <- sort(best[,"Hospital"])[1]
+    
     best
-    sort(best)
-
+    
 }
-
-
- 
